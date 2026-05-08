6afc601190a7a1d1a10439ec01e12d25ba8451c0
vicso-name <vic.sokoliuk@gmail.com>
2026-05-08 08:07:45 +0300

    Add PWA manifest and service worker

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "meal-flow-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
